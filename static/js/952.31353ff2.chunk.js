5eede14ceea7cc0bf3e761013e60eb6f0cd37bff
amplob <amplob@gmail.com>
2024-08-26 02:48:52 +0200

    Updates

---
+++ static/js/952.31353ff2.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmywebsite=self.webpackChunkmywebsite||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.31353ff2.chunk.js.map
